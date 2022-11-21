b24afdb8825d2a0542ebc476a106e954169481d8
30isnottoolate <akos.varga.92@gmail.com>
2022-11-21 19:36:00 +0100

    Add data fetching, local storage funcionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const FONT_SIZE_DEFAULT = 100;
@@ -9,14 +9,78 @@ const HOLD_TIME_DEFAULT = 2000;
 const ORIENTATION_DEFAULT = "horizontal"; // horizontal / vertical
 
 const colors = {
-	1: { "name": "red", "code": "#ffd6d9" },
-	2: { "name": "green", "code": "#b4f8ff" },
-	3: { "name": "blue", "code": "#99d3ff" },
-	4: { "name": "white", "code": "#ffffff" },
-	5: { "name": "yellow", "code": "#fff4ad" },
+    1: { "name": "red", "code": "#ffd6d9" },
+    2: { "name": "green", "code": "#b4f8ff" },
+    3: { "name": "blue", "code": "#99d3ff" },
+    4: { "name": "white", "code": "#ffffff" },
+    5: { "name": "yellow", "code": "#fff4ad" },
 }
 
 const App: React.FC = () => {
+    const [mode, setMode] = useState("start");
+    const [data, setData] = useState("");
+    const [textCount, setTextCount] = useState(0);
+    const [textIndex, setTextIndex] = useState(1);
+    const [settings, setSettings] = useState({
+        fontSize: FONT_SIZE_DEFAULT,
+        lineHeight: LINE_HEIGHT_DEFAULT,
+        colorIndex: COLOR_INDEX_DEFAULT,
+        textSpeed: TEXT_SPEED_DEFAULT,
+        holdButtonTime: HOLD_TIME_DEFAULT,
+        orientation: ORIENTATION_DEFAULT
+    });
+
+    useEffect(() => {
+        fetch('librarian.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (localStorage["fontSize"] && localStorage["lineHeight"] && localStorage["colorIndex"] &&
+                    localStorage["textSpeed"] && localStorage["holdButtonTime"] && localStorage["orientation"]) {
+                    setData(data);
+                    setTextCount(Object.keys(data.texts).length);
+                    setSettings({
+                        fontSize: parseInt(localStorage.getItem("fontSize") || FONT_SIZE_DEFAULT.toString()),
+                        lineHeight: parseFloat(localStorage.getItem("lineHeight") || LINE_HEIGHT_DEFAULT.toString()),
+                        colorIndex: parseInt(localStorage.getItem("colorIndex") || COLOR_INDEX_DEFAULT.toString()),
+                        textSpeed: parseInt(localStorage.getItem("textSpeed") || TEXT_SPEED_DEFAULT.toString()),
+                        holdButtonTime: parseInt(localStorage.getItem("holdButtonTime") || HOLD_TIME_DEFAULT.toString()),
+                        orientation: localStorage.getItem("orientation") || ORIENTATION_DEFAULT
+                    });
+                } else {
+                    setData(data);
+                    setTextCount(Object.keys(data.texts).length);
+                    defaultSettings();
+                }
+            })
+            .catch(() => console.log("Database missing."));
+    }, []);
+
+    const defaultLocalStorage = () => {
+        localStorage.setItem("fontSize", FONT_SIZE_DEFAULT.toString());
+        localStorage.setItem("lineHeight", LINE_HEIGHT_DEFAULT.toString());
+        localStorage.setItem("colorIndex", COLOR_INDEX_DEFAULT.toString());
+        localStorage.setItem("textSpeed", TEXT_SPEED_DEFAULT.toString());
+        localStorage.setItem("holdButtonTime", HOLD_TIME_DEFAULT.toString());
+        localStorage.setItem("orientation", ORIENTATION_DEFAULT);
+    }
+
+    const defaultSettings = () => {
+        setSettings({
+            fontSize: FONT_SIZE_DEFAULT,
+            lineHeight: LINE_HEIGHT_DEFAULT,
+            colorIndex: COLOR_INDEX_DEFAULT,
+            textSpeed: TEXT_SPEED_DEFAULT,
+            holdButtonTime: HOLD_TIME_DEFAULT,
+            orientation: ORIENTATION_DEFAULT
+        });
+        defaultLocalStorage();
+    }
+
     return (
         <>So it begins...</>
     )
