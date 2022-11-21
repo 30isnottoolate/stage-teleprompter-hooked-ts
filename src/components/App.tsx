8e5f7d3ae13b31344d1f6a51852dc6aceadc3d77
30isnottoolate <akos.varga.92@gmail.com>
2022-11-21 18:51:28 +0100

    Define constant values

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './App.css';
 
+const FONT_SIZE_DEFAULT = 100;
+const LINE_HEIGHT_DEFAULT = 1.2;
+const COLOR_INDEX_DEFAULT = 3;
+const TEXT_SPEED_DEFAULT = 100;
+const HOLD_TIME_DEFAULT = 2000;
+const ORIENTATION_DEFAULT = "horizontal"; // horizontal / vertical
+
+const colors = {
+	1: { "name": "red", "code": "#ffd6d9" },
+	2: { "name": "green", "code": "#b4f8ff" },
+	3: { "name": "blue", "code": "#99d3ff" },
+	4: { "name": "white", "code": "#ffffff" },
+	5: { "name": "yellow", "code": "#fff4ad" },
+}
+
 const App: React.FC = () => {
     return (
         <>So it begins...</>
