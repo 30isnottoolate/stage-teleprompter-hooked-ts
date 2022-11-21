baa5a841c5f53146fbfb4e6c7516e56a65805b87
30isnottoolate <akos.varga.92@gmail.com>
2022-11-21 18:40:10 +0100

    Create new component, fill in basic structure

---
+++ src/components/ControlButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import icons from '../utilities/icons';
+
+interface ControlButtonProps {
+    stateColor: string,
+    clickHandler: () => void,
+    mouseDownHandler: () => void,
+    mouseUpHandler: () => void,
+    icon: string
+}
+
+const ControlButton: React.FC<ControlButtonProps> = ({stateColor, clickHandler, mouseDownHandler, mouseUpHandler, icon}: ControlButtonProps) => {
+    return (
+        <button
+            style={{ borderColor: stateColor }}
+            onClick={clickHandler}
+            onMouseDown={mouseDownHandler}
+            onMouseUp={mouseUpHandler}
+        >
+            <svg
+                width={(icon === "selectSettings" || icon === "selectList") ? 70 * 2.25 : 70}
+                height={80}
+                fill={stateColor}
+                viewBox={`0 0 ${(icon === "selectSettings" || icon === "selectList") ? 36 : 16} 16`}
+            >
+                {icons[icon]}
+            </svg>
+        </button>
+    )
+}
+
+export default ControlButton;
