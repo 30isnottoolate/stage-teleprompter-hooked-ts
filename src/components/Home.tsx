6196f55fb22ad2ddbc865b32d6055c5d5285c70d
30isnottoolate <akos.varga.92@gmail.com>
2022-12-13 11:42:22 +0100

    Remove instructions from Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import useEventListener from '../utilities/useEventListener';
 import colors from '../utilities/colors';
-import Marker from './Marker';
 import ControlButton from './ControlButton';
 
 interface HomeProps {
@@ -19,38 +18,14 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({ settings, libraryStatus, fetchLibrary, setMode }: HomeProps) => {
-	const [helpIndex, setHelpIndex] = useState(1);
-	const [keyHold, setKeyHold] = useState(false);
-	const [keyDownTime, setKeyDownTime] = useState(0);
 
 	const handleKeyDown = (event: KeyboardEvent) => {
-		if (!keyHold) {
-			if (event.key === "a" || event.key === "b") {
-				setKeyHold(true);
-				setKeyDownTime((new Date()).getTime());
-			} else if (event.key === "c" && !event.repeat) {
-				handleButtonCDown();
-			}
-		}
-	}
-
-	const handleKeyUp = (event: KeyboardEvent) => {
-		if (keyHold) {
-			if (event.key === "a") {
-				if (((new Date()).getTime() - keyDownTime) > settings.holdButtonTime) {
-					handleButtonASet();
-				} else {
-					setKeyHold(false);
-					setKeyDownTime(0);
-				}
-			} else if (event.key === "b") {
-				if (((new Date()).getTime() - keyDownTime) > settings.holdButtonTime) {
-					handleButtonBList();
-				} else {
-					setKeyHold(false);
-					setKeyDownTime(0);
-				}
-			}
+		if (event.key === "a") {
+			handleButtonASet();
+		} else if (event.key === "b") {
+			handleButtonBList();
+		} else if (event.key === "c") {
+			handleButtonCInfo();
 		}
 	}
 
@@ -62,18 +37,10 @@ const Home: React.FC<HomeProps> = ({ settings, libraryStatus, fetchLibrary, setM
 		} else fetchLibrary();
 	}
 
-	const handleButtonCDown = () => {
-		if (helpIndex < 5) {
-			setHelpIndex((prevState) => prevState + 1);
-		} else {
-			setHelpIndex(1);
-		}
-	}
+	const handleButtonCInfo = () => setMode("info");
 
 	useEventListener("keydown", (event: KeyboardEvent) => handleKeyDown(event));
-	useEventListener("keyup", (event: KeyboardEvent) => handleKeyUp(event));
 
-	let listPos = (3 - helpIndex) * settings.fontSize * settings.lineHeight;
 	let stateColor = colors[settings.colorIndex].code;
 	let responsiveWidth = (settings.orientation === "vertical") ? "100vh" : "100vw";
 
@@ -87,38 +54,9 @@ const Home: React.FC<HomeProps> = ({ settings, libraryStatus, fetchLibrary, setM
 				lineHeight: settings.lineHeight
 			}}>
 			<p
-				id="head-line"
-				className={helpIndex <= 2 ? "visible" : "hidden"}>
-				KV Teleprompter
+				id="head-line">
+				KV Stage Teleprompter
 			</p>
-			<p
-				id="sub-line"
-				className={helpIndex === 1 ? "visible" : "hidden"}
-				style={{
-					position: "absolute",
-					left: (settings.fontSize * 0.69)
-				}}>
-				Control symbols:
-			</p>
-			<Marker
-				top={2 * settings.fontSize * settings.lineHeight}
-				left={settings.fontSize * 0.19}
-				fontSize={settings.fontSize}
-				lineHeight={settings.lineHeight}
-				stateColor={stateColor}
-			/>
-			<ul
-				style={{
-					top: listPos,
-					left: settings.fontSize * 0.69,
-					width: responsiveWidth
-				}}>
-				<li>&#9651;&#9661; - Previous / Next</li>
-				<li>&#9665;&#9655; - Change setting</li>
-				<li>&#9655;&#9634; - Start / Stop</li>
-				<li>&#9636; - Text List</li>
-				<li>&#8984; - Settings</li>
-			</ul>
 			<div id="control" style={{ width: responsiveWidth }}>
 				<ControlButton
 					fontSize={settings.fontSize}
@@ -135,8 +73,8 @@ const Home: React.FC<HomeProps> = ({ settings, libraryStatus, fetchLibrary, setM
 				<ControlButton
 					fontSize={settings.fontSize}
 					stateColor={stateColor}
-					mouseDownHandler={handleButtonCDown}
-					icon="down"
+					mouseDownHandler={handleButtonCInfo}
+					icon="info"
 				/>
 			</div>
 		</div>
