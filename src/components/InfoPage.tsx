81908f1fec31292d9a8baf328446d0f7b25e1dab
30isnottoolate <akos.varga.92@gmail.com>
2022-12-15 15:25:02 +0100

    Import and add Icons, overhaul component

--- src/components/InfoPage.tsx
+++ src/components/InfoPage.tsx
@@ -3,6 +3,7 @@ import useEventListener from '../utilities/useEventListener';
 import colors from '../utilities/colors';
 import Marker from './Marker';
 import ControlButton from './ControlButton';
+import Icon from './Icon';
 
 interface InfoPageProps {
     settings: {
@@ -35,12 +36,12 @@ const InfoPage: React.FC<InfoPageProps> = ({ settings, setMode }: InfoPageProps)
         if (infoIndex > 1) {
             setInfoIndex((prevState) => prevState - 1);
         } else {
-            setInfoIndex(5);
+            setInfoIndex(13);
         }
     }
 
     const handleButtonCDown = () => {
-        if (infoIndex < 5) {
+        if (infoIndex < 13) {
             setInfoIndex((prevState) => prevState + 1);
         } else {
             setInfoIndex(1);
@@ -49,7 +50,7 @@ const InfoPage: React.FC<InfoPageProps> = ({ settings, setMode }: InfoPageProps)
 
     useEventListener("keydown", (event: KeyboardEvent) => handleKeyDown(event));
 
-    let listPos = (2.5 - infoIndex) * settings.fontSize * settings.lineHeight;
+    let listPos = (2.75 - infoIndex) * settings.fontSize * settings.lineHeight;
     let stateColor = colors[settings.colorIndex].code;
     let responsiveWidth = (settings.orientation === "vertical") ? "100vh" : "100vw";
 
@@ -63,11 +64,12 @@ const InfoPage: React.FC<InfoPageProps> = ({ settings, setMode }: InfoPageProps)
                 lineHeight: settings.lineHeight
             }}>
             <p
-                className={"title " + (infoIndex <= 2 ? "visible" : "hidden")}>
+                className={"title " + (infoIndex <= 1 ? "visible" : "hidden")}
+                style={{ paddingTop: 0.25 * settings.fontSize * settings.lineHeight }} >
                 How to use?
             </p>
             <Marker
-                top={1.5 * settings.fontSize * settings.lineHeight}
+                top={1.75 * settings.fontSize * settings.lineHeight}
                 left={settings.fontSize * 0.19}
                 fontSize={settings.fontSize}
                 lineHeight={settings.lineHeight}
@@ -79,11 +81,97 @@ const InfoPage: React.FC<InfoPageProps> = ({ settings, setMode }: InfoPageProps)
                     left: settings.fontSize * 0.69,
                     width: responsiveWidth
                 }}>
-                <li>&#9651;&#9661; - Previous / Next</li>
-                <li>&#9665;&#9655; - Change setting</li>
-                <li>&#9655;&#9634; - Start / Stop</li>
-                <li>&#9636; - Text List</li>
-                <li>&#8984; - Settings</li>
+                <Icon
+                    icon="up"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="up"
+                />
+                <Icon
+                    icon="down"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="down"
+                />
+                <Icon
+                    icon="right"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="increase"
+                />
+                <Icon
+                    icon="left"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="decrease"
+                />
+                <Icon
+                    icon="play"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="play"
+                />
+                <Icon
+                    icon="pause"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="pause"
+                />
+                <Icon
+                    icon="next"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="next"
+                />
+                <Icon
+                    icon="home"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="home"
+                />
+                <Icon
+                    icon="list"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="library"
+                />
+                <Icon
+                    icon="settings"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="settings"
+                />
+                <Icon
+                    icon="select"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="select"
+                />
+                <Icon
+                    icon="refresh"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="refresh"
+                />
+                <Icon
+                    icon="info"
+                    fontSize={settings.fontSize}
+                    lineHeight={settings.lineHeight}
+                    stateColor={stateColor}
+                    text="info"
+                />
             </ul>
             <div id="control" style={{ width: responsiveWidth }}>
                 <ControlButton
