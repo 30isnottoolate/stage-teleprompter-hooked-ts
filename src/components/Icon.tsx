108bd7d2486ce8d100c9f91b10e1819c820fc947
30isnottoolate <akos.varga.92@gmail.com>
2022-12-15 12:18:38 +0100

    Create new component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import icons from '../utilities/icons';
+
+interface IconProps {
+    icon: string,
+    text: string,
+    fontSize: number,
+    lineHeight: number,
+    stateColor: string
+}
+
+const Icon: React.FC<IconProps> = ({ icon, text, fontSize, lineHeight, stateColor }: IconProps) => {
+    let padding = fontSize * (lineHeight - 1) / 2;
+
+    return (
+        <li>
+            <svg
+                className="text-marker"
+                height={fontSize}
+                fill={stateColor}
+                style={{ boxSizing: "content-box", padding: `${padding}px 0` }}
+                viewBox="0 0 16 16">
+                {icons[icon]}
+            </svg>
+            <pre>     - {text} </pre>
+        </li>
+    );
+}
+
+export default Icon;
