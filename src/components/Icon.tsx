bf7e40a7cb42eea29d30d16f93c78a9eabbac400
30isnottoolate <akos.varga.92@gmail.com>
2023-02-28 18:17:02 +0100

    Convert component to use rem values

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -13,13 +13,13 @@ const Icon: React.FC<IconProps> = ({ icon, text, fontSize, lineHeight, stateColo
     let padding = fontSize * (lineHeight - 1) / 2;
 
     return (
-        <li className="item-icon" style={{ height: fontSize * lineHeight }}>
+        <li className="item-icon" style={{ height: fontSize * lineHeight + "rem" }}>
             <svg
-                height={fontSize}
+                height={fontSize + "rem"}
                 fill={stateColor}
                 style={{
                     boxSizing: "content-box",
-                    padding: `${padding}px ${2 * padding}px`
+                    padding: `${padding}rem ${2 * padding}rem`
                 }}
                 viewBox="0 0 16 16">
                 {icons[icon]}
